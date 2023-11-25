44fe903ebbc4f51f4f26c9031ef9a9733c95c1f8
rohit.karbari <rohitkarbari26@gmail.com>
2023-11-25 23:00:38 +0530

    complete sign up functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,9 +2,9 @@ import User from "../Models/user.model.js";
 import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res, next) => {
-    const { userName, email, password } = req.body;
+    const { username, email, password } = req.body;
     const hanshedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({userName, email, password: hanshedPassword});
+    const newUser = new User({username, email, password: hanshedPassword});
     try {
         await newUser.save();
         res.status(201).json("user created succesfully"); 
