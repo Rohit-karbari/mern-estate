a3dbad45f397fba4cec25d82c914c9e7294a28c5
rohit.karbari <rohitkarbari26@gmail.com>
2023-12-28 06:17:50 +0530

    Clearded cookie

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -34,7 +34,8 @@ export const deleteUser = async (req, res, next) => {
 
     try {
         await User.findByIdAndDelete(req.params.id)
-        res.status(200).json("user has been deleted")
+        res.clearCookie("access_token")
+        res.status(200).json("user has been deleted!")
     } catch (error) {
         next(error)
     }
